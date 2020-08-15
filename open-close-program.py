847ad8dbc2a5040ff569ef2f7d1f1867eac2271a
gomezrondon <gomezrondon@gmail.com>
2020-08-15 15:38:09 -0600

    Opening and closing a file.

---
+++ open-close-program.py
@@ -0,0 +1,28 @@
+import os
+import pyautogui
+import time
+
+def openFile():
+    try:
+        os.startfile('D:\gomez\Documents\python-doc.docx')
+        time.sleep(4)
+        pyautogui.click(1027, 699)
+        time.sleep(1)
+        pyautogui.click(1027, 699)
+        # time.sleep(2)
+        # print(pyautogui.position()) #Point(x=1027, y=699)
+
+    except Exception as e:
+        print(str(e))
+
+
+def closeFile():
+    try:
+        os.system('TASKKILL /F /IM soffice.bin') #is libreOffice.exe
+
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+    openFile()
+    # closeFile()
