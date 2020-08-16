9f579634d54fdb89424e220b82ed2126e73160b1
gomezrondon <gomezrondon@gmail.com>
2020-08-16 09:56:04 -0600

    Adding paste text and translated text to de entry.

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def getConfig(f_path):
                config.append(each.replace("\n",""))
      return config
 
+def getTextFromFile(f_path):
+     lines = []
+     with open(f_path, "rt") as t_file:
+          for each in t_file:
+               lines.append(each)
+     return lines
+
+
+def writeTextToWord(paste_text):
+     for line in paste_text:
+          paragraphs[0].add_run(line)
+     paragraphs[0].add_run('\n')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -29,15 +42,26 @@ if __name__ == '__main__':
      paragraphs  = doc.paragraphs
      text = paragraphs[0].text
      paragraphs[0]._p.clear()
+     paste_text = getTextFromFile(config[2])
+     t_paste_text = getTextFromFile(config[3])
+     # image_path = r'D:\temp\oct-tesseract\image\paste.png'
 
      paragraphs[0].add_run('------------1---------> '+str(datetime.datetime.now().today().strftime('%Y-%m-%d  time: %H:%M:%S'))+' \n')
      paragraphs[0].add_run('\n')
-     paragraphs[0].add_run(' Nueva entrada de texto!! \n')
+
+     if len(paste_text) > 0:
+          paragraphs[0].add_run(' ** Original text ** \n')
+          writeTextToWord(paste_text)
+          paragraphs[0].add_run(' ** Translated text ** \n')
+          writeTextToWord(t_paste_text)
+     else:
+          paragraphs[0].add_run(' Nueva entrada de texto!! \n')
+          paragraphs[0].add_run('\n')
+          paragraphs[0].add_run('\n')
+          paragraphs[0].add_run('\n')
+
      paragraphs[0].add_run('\n')
-     paragraphs[0].add_run('\n')
-     paragraphs[0].add_run('\n')
-     paragraphs[0].add_run('\n')
-     #paragraphs[1].add_run('-----------------------------------< \n')
+
      paragraphs[0].add_run(text)
      doc.save(filePath)
 
